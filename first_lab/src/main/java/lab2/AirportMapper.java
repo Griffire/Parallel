42dfe1c1d388f286779c5898c37add23a83249a4
Griffire <eaglegriff@mail.ru>
2020-10-13 20:56:20 +0300

    Scripted auto-commit on change (2020-10-13 20:56:20) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportMapper.java
+++ first_lab/src/main/java/lab2/AirportMapper.java
@@ -14,7 +14,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable>
             InterruptedException {
         String line = value.toString().toLowerCase();
         String [] words = line.split("[, ]");
-        context.write(new Text(words[7]),new IntWwitable(1));
+        context.write(new Text(words[7]),new IntWritable(1));
 //        for (String word : words) {
 //            context.write(new Text(word), new IntWritable(1));
 //        }
