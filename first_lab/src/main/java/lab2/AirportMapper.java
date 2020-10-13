389d44aedfc76a05387c3518dfb24a21da72398f
Griffire <eaglegriff@mail.ru>
2020-10-13 21:59:06 +0300

    Scripted auto-commit on change (2020-10-13 21:59:06) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportMapper.java
+++ first_lab/src/main/java/lab2/AirportMapper.java
@@ -14,8 +14,8 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, FloatWritabl
             InterruptedException {
         String line = value.toString();
         String [] words = line.split("[ , ]");
-        if (words[0] != "YEAR") {
-            context.write(new Text(words[13]), new FloatWritable(Float.parseFloat("103")));
+        if (words[14] != "YEAR") {
+            context.write(new Text(words[14]), new FloatWritable(Float.parseFloat("103")));
 
             //context.write(new Text(words[13]), new FloatWritable(Float.parseFloat(words[17])));
         }
