e11c1a18b6b0df03e1746c80a2c83a9e0f9ab472
Griffire <eaglegriff@mail.ru>
2020-10-13 21:46:58 +0300

    Scripted auto-commit on change (2020-10-13 21:46:58) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportReducer.java
+++ first_lab/src/main/java/lab2/AirportReducer.java
@@ -14,7 +14,7 @@ public class AirportReducer extends Reducer<Text, FloatWritable, Text, LongWrita
         long count=0;
         float average = (float) 0.0;
         for (FloatWritable v : values) {
-            average = average * count + (float)v;
+            average = average * count + get(v);
             count++;
             average /= count;
         }
