882fab507d78b809950ddeb65a60d0a7eb74e325
Griffire <eaglegriff@mail.ru>
2020-10-13 22:03:42 +0300

    Scripted auto-commit on change (2020-10-13 22:03:42) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportMapper.java
+++ first_lab/src/main/java/lab2/AirportMapper.java
@@ -15,7 +15,8 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, FloatWritabl
         String line = value.toString();
         String [] words = line.split("[ , ]");
         if (!words[14].equals("DEST_AIRPORT_ID")) {
-            context.write(new Text(words[14]), new FloatWritable(Float.parseFloat(words[17])));
+           // if (words[17].equa)
+            context.write(new Text(words[14]), new FloatWritable(Float.parseFloat("0.3")));
 
             //context.write(new Text(words[13]), new FloatWritable(Float.parseFloat(words[17])));
         }
