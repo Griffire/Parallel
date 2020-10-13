c13ed4c11c6fc42a1e3e929e6691bf45bd377c4f
Griffire <eaglegriff@mail.ru>
2020-10-13 21:03:54 +0300

    Scripted auto-commit on change (2020-10-13 21:03:54) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportMapper.java
+++ first_lab/src/main/java/lab2/AirportMapper.java
@@ -12,8 +12,8 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable>
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
-        String line = value.toString().toLowerCase();
-        String [] words = line.split("[, ]");
+        String line = value.toString();
+        String [] words = line.split("[ , ]");
         context.write(new Text(words[7]),new IntWritable(1));
 //        for (String word : words) {
 //            context.write(new Text(word), new IntWritable(1));
