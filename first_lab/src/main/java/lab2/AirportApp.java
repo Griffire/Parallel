9c348a0af713d60ef1c952eb3d7828272b234586
Griffire <eaglegriff@mail.ru>
2020-10-13 21:44:48 +0300

    Scripted auto-commit on change (2020-10-13 21:44:48) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportApp.java
+++ first_lab/src/main/java/lab2/AirportApp.java
@@ -4,7 +4,7 @@ package lab2;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 
