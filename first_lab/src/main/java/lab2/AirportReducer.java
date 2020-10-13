12b5f323b070c3fffcef917e5cd0dc607261df01
Griffire <eaglegriff@mail.ru>
2020-10-13 21:39:06 +0300

    Scripted auto-commit on change (2020-10-13 21:39:05) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportReducer.java
+++ first_lab/src/main/java/lab2/AirportReducer.java
@@ -2,7 +2,7 @@ package lab2;
 
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 
 import java.io.IOException;
