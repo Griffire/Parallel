874102021d069422183f1af9fad4b661e2a5d860
Griffire <eaglegriff@mail.ru>
2020-10-13 20:22:05 +0300

    Scripted auto-commit on change (2020-10-13 20:22:05) by gitwatch.sh

---
+++ first_lab/src/main/java/lab2/AirportMapper.java
@@ -0,0 +1,23 @@
+package lab2;
+
+
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+
+import java.io.IOException;
+
+public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException,
+            InterruptedException {
+        String line = value.toString().toLowerCase();
+        String [] words = line.split("[, ]");
+        context.write(new Text(words[7],new IntWwitable(1)))
+//        for (String word : words) {
+//            context.write(new Text(word), new IntWritable(1));
+//        }
+
+    }
+}
