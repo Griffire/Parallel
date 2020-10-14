15a0fef15a08b418913752cd26c56ccee762850b
Griffire <eaglegriff@mail.ru>
2020-10-14 11:33:27 +0300

    Scripted auto-commit on change (2020-10-14 11:33:27) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportReducer.java
+++ first_lab/src/main/java/lab2/AirportReducer.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.io.LongWritable;
 
 import java.io.IOException;
 
-public class AirportReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
+public class AirportReducer extends Reducer<Text, FloatWritable, Text,Text> {
     @Override
     protected void reduce(Text key, Iterable<FloatWritable> values, Context context) throws
             IOException, InterruptedException {
