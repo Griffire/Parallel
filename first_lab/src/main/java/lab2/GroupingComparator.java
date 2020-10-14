85e0155d8dc0029acfe7a73a55208ad0037a488b
Griffire <eaglegriff@mail.ru>
2020-10-14 12:36:02 +0300

    Scripted auto-commit on change (2020-10-14 12:36:02) by gitwatch.sh

---
+++ first_lab/src/main/java/lab2/GroupingComparator.java
@@ -0,0 +1,18 @@
+package lab2;
+
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class GroupComparator extends WritableComparator {
+    protected GroupComparator() {
+        super(CustomKey.class, true);
+    }
+
+    @Override
+    public int compare(WritableComparable w1, WritableComparable w2) {
+        CustomKey ck = (CustomKey) w1;
+        CustomKey ck2 = (CustomKey) w2;
+        return CustomKey.compare(ck.getUserId(), ck2.getUserId());
+    }
+}
