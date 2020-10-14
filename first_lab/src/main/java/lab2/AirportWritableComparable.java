248f8d248ffc0e28dcd56413d48dc9032cd169df
Griffire <eaglegriff@mail.ru>
2020-10-14 12:41:04 +0300

    Scripted auto-commit on change (2020-10-14 12:41:04) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportWritableComparable.java
+++ first_lab/src/main/java/lab2/AirportWritableComparable.java
@@ -36,7 +36,11 @@ public class AirportWritableComparable implements WritableComparable {
         }
     }
 
-//    public int hashCode() {
+    public String getName() {
+        return name;
+    }
+
+    //    public int hashCode() {
 //        final int prime = 31;
 //        int result = 1;
 //        result = prime * result + name;//  !
--- first_lab/src/main/java/lab2/GroupingComparator.java
+++ first_lab/src/main/java/lab2/GroupingComparator.java
@@ -6,13 +6,11 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class GroupComparator extends WritableComparator {
     protected GroupComparator() {
-        super(CustomKey.class, true);
+        super(AirportWritableComparable.class, true);
     }
 
     @Override
     public int compare(WritableComparable w1, WritableComparable w2) {
-        CustomKey ck = (CustomKey) w1;
-        CustomKey ck2 = (CustomKey) w2;
-        return CustomKey.compare(ck.getUserId(), ck2.getUserId());
+        return w1.getName().compareTo(w2.getName());
     }
 }
