16704c82f79b7612a448756e93aa2d32af05bd53
Griffire <eaglegriff@mail.ru>
2020-10-15 15:33:06 +0300

    Scripted auto-commit on change (2020-10-15 15:33:06) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportMapper.java
+++ first_lab/src/main/java/lab2/AirportMapper.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<AirportWritableComparable, Text, Text, Text> {
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException,
+    protected void map(AirportWritableComparable key, Text value, Context context) throws IOException,
             InterruptedException {
         String line = value.toString();
         String [] words = line.split("[ , ]");
