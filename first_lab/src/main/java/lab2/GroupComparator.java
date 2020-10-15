95237fb3f1c1bfcc9a1b00a162f122d299902e19
Griffire <eaglegriff@mail.ru>
2020-10-15 19:45:00 +0300

    Scripted auto-commit on change (2020-10-15 19:45:00) by gitwatch.sh

--- first_lab/src/main/java/lab2/GroupComparator.java
+++ first_lab/src/main/java/lab2/GroupComparator.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class GroupComparator extends WritableComparator {
     protected GroupComparator() {
-       // super(AirportWritableComparable.class, true);
+        super(AirportWritableComparable.class, true);
     }
 
 
