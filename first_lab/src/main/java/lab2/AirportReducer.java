09c31ef0c995f022f28057d80ea93c6ad7fe1b8f
Griffire <eaglegriff@mail.ru>
2020-10-15 20:14:18 +0300

    Scripted auto-commit on change (2020-10-15 20:14:18) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportReducer.java
+++ first_lab/src/main/java/lab2/AirportReducer.java
@@ -6,19 +6,24 @@ import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text,Text> {
     @Override
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws
             IOException, InterruptedException {
 
+        Iterator<Text> v = values.iterator();
         long count=0;
         float average = (float) 0.0;
         float min = Float.MAX_VALUE;
         float max = (float)(-1.0) * Float.MAX_VALUE;
-        String name = "";
+        String name = v.iterNext().toString();
+
+        while ()
         for (Text v : values) {
-            if (key.getCode().charAt(0) == '1') {
+
+           // if (key.getCode().charAt(0) == '1') {
                 float i = Float.parseFloat(v.toString());
                 average = average * count + i;
                 count++;
