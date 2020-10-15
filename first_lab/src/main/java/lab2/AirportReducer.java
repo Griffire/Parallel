e7028d6048f96aa33ad0895de81ebed1f601ad6a
Griffire <eaglegriff@mail.ru>
2020-10-15 17:17:37 +0300

    Scripted auto-commit on change (2020-10-15 17:17:37) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportReducer.java
+++ first_lab/src/main/java/lab2/AirportReducer.java
@@ -18,10 +18,11 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
         float max = (float)(-1.0) * Float.MAX_VALUE;
         String name = "";
         for (Text v : values) {
+            count++;
             if (key.getCode().charAt(0) == '1') {
                 float i = Float.parseFloat(v.toString());
                 average = average * count + i;
-                count++;
+
                 average /= (float) count;
                 if (i > max) {
                     max = i;
