945a7a854772806b826e683ba0db9094ee33a2eb
Griffire <eaglegriff@mail.ru>
2020-10-15 17:20:51 +0300

    Scripted auto-commit on change (2020-10-15 17:20:51) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportPartitioner.java
+++ first_lab/src/main/java/lab2/AirportPartitioner.java
@@ -6,8 +6,9 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class AirportPartitioner extends Partitioner<AirportWritableComparable, Text> {
 
     @Override
-    public int getPartition(AirportWritableComparable key, Text value, int numPartitions) {
-        return (key.getName().hashCode() & Integer.MAX_VALUE) % numPartitions;
+    public int getPartition(WritableComparable key, Text value, int numPartitions) {
+        AirportWritableComparable o = (AirportWritableComparable) key;
+        return (o.getName().hashCode() & Integer.MAX_VALUE) % numPartitions;
     }
 
 }
