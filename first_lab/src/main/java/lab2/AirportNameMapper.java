a4323d4c2ee96320fb355eca521b27eec797e193
Griffire <eaglegriff@mail.ru>
2020-10-17 16:13:32 +0300

    Scripted auto-commit on change (2020-10-17 16:13:32) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportNameMapper.java
+++ first_lab/src/main/java/lab2/AirportNameMapper.java
@@ -9,11 +9,13 @@ import org.apache.hadoop.io.LongWritable;
 import java.io.IOException;
 
 public class AirportNameMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+    public static final String NAME_PATTERN = "[,]";
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
         String line = value.toString();
-        String [] words = line.split(",");
+        String [] words = line.split(NAME_PATTERN);
         if (words.length > 2) {
             context.write(new AirportWritableComparable(words[0], "0"), new Text(words[1] + "," + words[2]));
         } else if (words.length == 2){
