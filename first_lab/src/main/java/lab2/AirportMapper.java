35941eecd9e00dec0a019c51f242010c9e4e85c2
Griffire <eaglegriff@mail.ru>
2020-10-17 16:11:36 +0300

    Scripted auto-commit on change (2020-10-17 16:11:36) by gitwatch.sh

--- first_lab/src/main/java/lab2/AirportMapper.java
+++ first_lab/src/main/java/lab2/AirportMapper.java
@@ -9,11 +9,13 @@ import org.apache.hadoop.io.LongWritable;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+    public static final String DELAY_PATTERN = "[ ,]";
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
         String line = value.toString();
-        String [] words = line.split("[ ,]");
+        String [] words = line.split(DELAY_PATTERN);
 
         if (words[14].length() < 7 && words[18].length() > 0 && Float.parseFloat(words[18]) != (float) 0.0) {
             context.write(new AirportWritableComparable("\"" + words[14] + "\"","1"), new Text(words[18]));
