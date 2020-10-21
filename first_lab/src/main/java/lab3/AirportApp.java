a3fc73de7364d154ec81ff7e2fe4a3cfb67b26ae
Griffire <eaglegriff@mail.ru>
2020-10-21 20:09:39 +0300

    Scripted auto-commit on change (2020-10-21 20:09:39) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp.java
+++ first_lab/src/main/java/lab3/AirportApp.java
@@ -6,20 +6,12 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportApp {
-    private JavaSparkContext sc;
 
     public static void main(String[] args) throws Exception {
-       setup(args);
-       execute();
-    }
-
-    public void setup(String[] args){
+        JavaSparkContext sc;
         SparkConf sparkConf = new SparkConf().setAppName("Lab3");
         sc = new JavaSparkContext(sparkConf);
     }
 
-    public void execute(){
-
-    }
 
 }
