498c94534a10a3e19b42b270c86ccd27dfbfd942
Griffire <eaglegriff@mail.ru>
2020-10-21 20:11:49 +0300

    Scripted auto-commit on change (2020-10-21 20:11:49) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp.java
+++ first_lab/src/main/java/lab3/AirportApp.java
@@ -14,6 +14,9 @@ public class AirportApp {
         sc = new JavaSparkContext(sparkConf);
      //   String inputFile = "";
         JavaRDD<String> distFile = sc.textFile( "war-and-peace-1.txt");
+        JavaRDD<String> splitted = distFile.flatMap(Hadoop
+                s -> Arrays.stream(Hadoop s.split(Hadoop " ")).iterator(Hadoop )
+        );
 
     }
 
