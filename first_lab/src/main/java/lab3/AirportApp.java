aa4223ab144c9b1bc010cb2ae44a810bf4fb0316
Griffire <eaglegriff@mail.ru>
2020-10-21 20:30:26 +0300

    Scripted auto-commit on change (2020-10-21 20:30:26) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp.java
+++ first_lab/src/main/java/lab3/AirportApp.java
@@ -22,7 +22,7 @@ public class AirportApp {
                 s -> Arrays.stream(s.split( " ")).iterator());
         JavaPairRDD<String, Long> wordsWithCount = splitted.mapToPair(
                 s -> new Tuple2<>(s, 1l) );
-        wordsWithCount.saveAsHadoopFile("war-and-peace-2.txt");
+        wordsWithCount.saveAsHadoopFile("war-and-peace-2.txt", String.class, Long.class, );
         //JavaPairRDD<String,Long>
 
     }
