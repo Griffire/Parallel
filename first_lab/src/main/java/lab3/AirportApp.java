69eded806ef39dc9a5177b3cba9d90087e32e354
Griffire <eaglegriff@mail.ru>
2020-10-21 20:26:48 +0300

    Scripted auto-commit on change (2020-10-21 20:26:48) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp.java
+++ first_lab/src/main/java/lab3/AirportApp.java
@@ -22,6 +22,7 @@ public class AirportApp {
                 s -> Arrays.stream(s.split( " ")).iterator());
         JavaPairRDD<String, Long> wordsWithCount = splitted.mapToPair(
                 s -> new Tuple2<>(s, 1l) );
+        wordsWithCount.sa
         //JavaPairRDD<String,Long>
 
     }
