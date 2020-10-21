81c2806ecc8e7b756fd52279241694f2782d1c6a
Griffire <eaglegriff@mail.ru>
2020-10-21 20:12:17 +0300

    Scripted auto-commit on change (2020-10-21 20:12:17) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp.java
+++ first_lab/src/main/java/lab3/AirportApp.java
@@ -6,6 +6,8 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.util.Arrays;
+
 public class AirportApp {
 
     public static void main(String[] args) throws Exception {
