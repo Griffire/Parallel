7dad17f9495a38d7f3cd0dea08ffcd9f0dc6f6d7
Griffire <eaglegriff@mail.ru>
2020-10-21 20:05:37 +0300

    Scripted auto-commit on change (2020-10-21 20:05:37) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp.java
+++ first_lab/src/main/java/lab3/AirportApp.java
@@ -11,7 +11,7 @@ public class AirportApp {
     }
 
     public void setup(String[] args){
-        SparkConf sparkConf = new SparkConf().setAppName("M")
+        SparkConf sparkConf = new SparkConf().setAppName("")
     }
 
 }
