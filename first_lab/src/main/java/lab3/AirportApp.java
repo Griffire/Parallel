c60751526335c0216ed576f15bd67a5c0a4564a3
Griffire <eaglegriff@mail.ru>
2020-10-21 20:03:38 +0300

    Scripted auto-commit on change (2020-10-21 20:03:38) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp.java
+++ first_lab/src/main/java/lab3/AirportApp.java
@@ -4,9 +4,7 @@ import org.apache.spark.mapred. ;
 
 public class AirportApp {
     public static void main(String[] args) throws Exception {
-        sparkContext.textFile("hdfs://...")
-                .flatMap(line => line.split(" "))
-            .map(word => (word, 1)).reduceByKey(_ + _)
-                .saveAsTextFile("hdfs://...")
+       setup(args);
+       
     }
 }
