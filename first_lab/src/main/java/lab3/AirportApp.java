fa69140592ec5e19d56f1f2107c8cf20b1491d28
Griffire <eaglegriff@mail.ru>
2020-10-23 18:49:13 +0300

    Scripted auto-commit on change (2020-10-23 18:49:13) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp.java
+++ first_lab/src/main/java/lab3/AirportApp.java
@@ -1,32 +1,32 @@
 package lab3;
 
-//import org.apache.spark.mapred. ;
+import org.apache.spark.mapred. ;
 
-//import org.apache.hadoop.mapred.TextOutputFormat;
-//import org.apache.spark.SparkConf;
-//import org.apache.spark.api.java.JavaPairRDD;
-//import org.apache.spark.api.java.JavaRDD;
-//import org.apache.spark.api.java.JavaSparkContext;
-//import scala.Tuple2;
-//
-//import java.util.Arrays;
-//
-//public class AirportApp {
-//
-//    public static void main(String[] args)  {
-//        JavaSparkContext sc;
-//        SparkConf sparkConf = new SparkConf().setAppName("AirportApp");
-//        sc = new JavaSparkContext(sparkConf);
-//     //   String inputFile = "";
-//        JavaRDD<String> distFile = sc.textFile( "warandpeace-1.txt");
-//        JavaRDD<String> splitted = distFile.flatMap(
-//                s -> Arrays.stream(s.split( " ")).iterator());
-//        JavaPairRDD<String, Long> wordsWithCount = splitted.mapToPair(
-//                s -> new Tuple2<>(s, 1l) );
-//        wordsWithCount.saveAsHadoopFile("war-and-peace-2.txt", String.class, Long.class, TextOutputFormat.class);
-//        //JavaPairRDD<String,Long>
-//
-//    }
-//}
+import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+public class AirportApp {
+
+    public static void main(String[] args)  {
+        JavaSparkContext sc;
+        SparkConf sparkConf = new SparkConf().setAppName("AirportApp");
+        sc = new JavaSparkContext(sparkConf);
+     //   String inputFile = "";
+        JavaRDD<String> distFile = sc.textFile( "warandpeace-1.txt");
+        JavaRDD<String> splitted = distFile.flatMap(
+                s -> Arrays.stream(s.split( " ")).iterator());
+        JavaPairRDD<String, Long> wordsWithCount = splitted.mapToPair(
+                s -> new Tuple2<>(s, 1l) );
+       //wordsWithCount.saveAsHadoopFile("war-and-peace-2.txt", String.class, Long.class, TextOutputFormat.class);
+        //JavaPairRDD<String,Long>
+
+    }
+}
 
 
