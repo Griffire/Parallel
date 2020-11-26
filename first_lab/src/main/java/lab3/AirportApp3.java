2061259568cf9ce0b5c59bd0f8439119f9cb32e6
Aleksandr Lezhebokov <eaglegriff@gmail.com>
2020-11-26 21:51:39 +0300

    Scripted auto-commit on change (2020-11-26 21:51:39) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp3.java
+++ first_lab/src/main/java/lab3/AirportApp3.java
@@ -46,7 +46,9 @@ public class AirportApp3 {
         final Broadcast<Map<String, String>> airportsBroadcasted =
                 sc.broadcast(AIRPORT_NAMES);
 
-        JavaPairRDD<Tuple2<String,String>, Times> fDelay = fAIRPORT_TIMES.map( s -> s.split(",")).mapToPair(strings -> new Tuple2<>(new Tuple2<>(strings[], strings[]), new Times(strings[],strings[])))
+        JavaPairRDD<Tuple2<String,String>, Times> fDelay = fAIRPORT_TIMES.map( s -> s.split(",")).mapToPair(strings ->
+                new Tuple2<>(new Tuple2<>(strings[1], strings[1]),
+                new Times(strings[1],strings[1])));
 
 //        JavaPairRDD<String, Long> wordsWithCount = splitted.mapToPair(
 //                s -> new Tuple2<>(s, 1l) );
