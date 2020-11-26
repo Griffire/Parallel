5be516e0241da5cf894ab8b9c6286f5d4c850445
Aleksandr Lezhebokov <eaglegriff@gmail.com>
2020-11-26 20:42:23 +0300

    Scripted auto-commit on change (2020-11-26 20:42:23) by gitwatch.sh

--- first_lab/src/main/java/lab3/Times.java
+++ first_lab/src/main/java/lab3/Times.java
@@ -16,7 +16,7 @@ public class Times implements Serializable{
         this.canceledN = 1;
     }
 
-    public Times counting (){
-        
+    public Times counting (Iterable<Times> time){
+
     }
 }
