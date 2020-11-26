2fc3c77550b38debbf30c30d8e7dee09d86893f8
Aleksandr Lezhebokov <eaglegriff@gmail.com>
2020-11-26 17:43:00 +0300

    Scripted auto-commit on change (2020-11-26 17:43:00) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp3.java
+++ first_lab/src/main/java/lab3/AirportApp3.java
@@ -24,7 +24,7 @@ public class AirportApp3 {
                 s -> Arrays.stream(s.split( " ")).iterator());
         JavaPairRDD<String, Long> wordsWithCount = splitted.mapToPair(
                 s -> new Tuple2<>(s, 1l) );
-
+        JavaRDD<String> output = wordsWithCount.map()
         System.out.println("Nice!\n\n");
        //wordsWithCount.saveAsHadoopFile("war-and-peace-2.txt", String.class, Long.class, TextOutputFormat.class);
         //JavaPairRDD<String,Long>
