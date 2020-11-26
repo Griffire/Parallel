9cd4b475f5a67ee7185ba4cafb4ef89e96ed9dba
Aleksandr Lezhebokov <eaglegriff@gmail.com>
2020-11-26 20:31:53 +0300

    Scripted auto-commit on change (2020-11-26 20:31:53) by gitwatch.sh

--- first_lab/src/main/java/lab3/Times.java
+++ first_lab/src/main/java/lab3/Times.java
@@ -3,9 +3,11 @@ package lab3;
 import java.io.Serializable;
 
 public class Times implements Serializable{
+    private float delay;
+    private int canceled;
     Times(){}
 
     Times(String time, String canceled){
-        
+
     }
 }
