56b37382f0c93883bbe341be78717d2ec4c0a84e
Aleksandr Lezhebokov <eaglegriff@gmail.com>
2020-11-28 16:20:50 +0300

    Scripted auto-commit on change (2020-11-28 16:20:50) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp3.java
+++ first_lab/src/main/java/lab3/AirportApp3.java
@@ -51,7 +51,7 @@ public class AirportApp3 {
                 new Times(strings[18],strings[19]))).groupByKey().mapValues(s -> new Times().counting(s));
 
         JavaRDD<String> output = fDelay.map(s -> {
-            String str = "NameFrom : " + 
+            String str = "NameFrom : " + airportsBroadcasted
                  }) ;
         output.saveAsTextFile("output");
 
