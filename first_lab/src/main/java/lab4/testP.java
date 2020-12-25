b4dc495b1374404d0a1207f6246fb03a6ae69143
Griffire <eaglegriff@mail.ru>
2020-12-25 22:39:13 +0300

    Scripted auto-commit on change (2020-12-25 22:39:13) by gitwatch.sh

--- first_lab/src/main/java/lab4/testP.java
+++ first_lab/src/main/java/lab4/testP.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class testP {
     private String testName, testExResult;
-    private ArrayList<Integer> params;
+    private ArrayList<Object> params;
 
     public String getTestName() {
         return this.testName;
