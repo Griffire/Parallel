796bf327772a59bd3b47452c0271abb38f513bc6
Griffire <eaglegriff@mail.ru>
2020-12-25 23:06:20 +0300

    Scripted auto-commit on change (2020-12-25 23:06:20) by gitwatch.sh

--- first_lab/src/main/java/lab4/StoreActor.java
+++ first_lab/src/main/java/lab4/StoreActor.java
@@ -19,6 +19,7 @@ public class StoreActor extends AbstractActor {
     }
 
     private void insert(MessageP msg){
-        ArrayList<String>  results = actorStorage.get()
+        ArrayList<String>  results = actorStorage.get(msg.getIdS())
+                
     }
 }
