0e51ab0df655dce024ccb4c85626a9301c0c2bcb
Griffire <eaglegriff@mail.ru>
2020-12-25 19:19:18 +0300

    Scripted auto-commit on change (2020-12-25 19:19:18) by gitwatch.sh

--- first_lab/src/main/java/lab4/ParseApp.java
+++ first_lab/src/main/java/lab4/ParseApp.java
@@ -4,6 +4,7 @@ import akka.actor.*;
 import akka.http.*;
 import akka.http.javadsl.Http;
 import akka.routing.RouterActor;
+import akka.stream.javadsl.Flow;
 import org.apache.http.protocol.HTTP;
 import org.spark_project.jetty.http.HttpParser;
 
@@ -15,7 +16,7 @@ public class ParseApp {
         ActorRef r1Actor = s1.actorOf(Props.create(RouterActor.class), "Router1");
         Http http = Http.get(s1);
         ParsingModule PM = new ParsingModule(r1Actor);
-
+        Flow
 
     }
 }
