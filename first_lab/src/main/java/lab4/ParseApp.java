6f46b72d3e5de1dbdc1688d746d9e75508a1fb82
Griffire <eaglegriff@mail.ru>
2020-12-25 17:50:04 +0300

    Scripted auto-commit on change (2020-12-25 17:50:04) by gitwatch.sh

--- first_lab/src/main/java/lab4/ParseApp.java
+++ first_lab/src/main/java/lab4/ParseApp.java
@@ -9,8 +9,8 @@ public class ParseApp {
 
     public static void main(String[] args) throws Exception{
         ActorSystem s1 = ActorSystem.create("test1");
-        ActorRef routerActor = s1.actorOf(Props.create(RouterActor.class), "Router1")
-                ;
+        ActorRef routerActor = s1.actorOf(Props.create(RouterActor.class), "Router1");
+        
 
     }
 }
