d4606c9d3699e3ba403565fba44e029a29eebbb8
Griffire <eaglegriff@mail.ru>
2020-12-25 21:55:20 +0300

    Scripted auto-commit on change (2020-12-25 21:55:20) by gitwatch.sh

--- first_lab/src/main/java/lab4/MessageP.java
+++ first_lab/src/main/java/lab4/MessageP.java
@@ -1,9 +1,12 @@
 package lab4;
 
+import java.util.ArrayList;
+
 public class MessageP {
     private final String idS;
+    private ArrayList<testP> tests;
 
-    MessageP(String idS, ArrayList<TestData> tests){
+    MessageP(String idS, ArrayList<testP> tests){
         this.idS = idS;
         this.tests = tests;
     }
