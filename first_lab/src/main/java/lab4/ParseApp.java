892fa1f505c72ae4056c97cedcab085ae5e81a0d
Griffire <eaglegriff@mail.ru>
2020-12-25 17:44:04 +0300

    Scripted auto-commit on change (2020-12-25 17:44:04) by gitwatch.sh

--- first_lab/src/main/java/lab4/ParseApp.java
+++ first_lab/src/main/java/lab4/ParseApp.java
@@ -3,11 +3,10 @@ package lab4;
 import akka.actor.*;
 import akka.*;
 
-
 public class ParseApp {
 
     public static void main(String[] args) {
         ActorSystem s1 = ActorSystem.create("test1");
-        
+
     }
 }
