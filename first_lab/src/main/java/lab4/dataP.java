4b2ddf3edad41354dbc8d43b406fafd5d07e3081
Griffire <eaglegriff@mail.ru>
2020-12-25 19:16:03 +0300

    Scripted auto-commit on change (2020-12-25 19:16:03) by gitwatch.sh

--- first_lab/src/main/java/lab4/dataP.java
+++ first_lab/src/main/java/lab4/dataP.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 
 public class dataP {
-    @JsonProperty("") String 
+    @JsonProperty("idS") String idS;
+    
 
 }
