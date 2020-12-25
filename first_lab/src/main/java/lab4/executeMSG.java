bb84350c7064703c34ee50766a546b2f3a93509a
Griffire <eaglegriff@mail.ru>
2020-12-25 22:25:53 +0300

    Scripted auto-commit on change (2020-12-25 22:25:53) by gitwatch.sh

--- first_lab/src/main/java/lab4/executeMSG.java
+++ first_lab/src/main/java/lab4/executeMSG.java
@@ -5,6 +5,38 @@ import java.util.ArrayList;
 public class executeMSG {
     private  ArrayList<Object> params;
     private String idS , function, jsS , testName ,  testExResult;
-    
 
+
+    public executeMSG(ArrayList<Object> params, String idS, String function, String jsS, String testName, String testExResult) {
+        this.params = params;
+        this.idS = idS;
+        this.function = function;
+        this.jsS = jsS;
+        this.testName = testName;
+        this.testExResult = testExResult;
+    }
+
+    public ArrayList<Object> getParams() {
+        return this.params;
+    }
+
+    public String getIdS() {
+        return this.idS;
+    }
+
+    public String getFunction() {
+        return this.function;
+    }
+
+    public String getJsS() {
+        return this.jsS;
+    }
+
+    public String getTestName() {
+        return this.testName;
+    }
+
+    public String getTestExResult() {
+        return this.testExResult;
+    }
 }
