67b1d9ef96afc055448e09e63cc297f83c730e60
Griffire <eaglegriff@mail.ru>
2020-12-25 19:14:30 +0300

    Scripted auto-commit on change (2020-12-25 19:14:30) by gitwatch.sh

--- first_lab/src/main/java/lab4/dataP.java
+++ first_lab/src/main/java/lab4/dataP.java
@@ -1,5 +1,11 @@
 package lab4;
 
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
 public class dataP {
 
 
