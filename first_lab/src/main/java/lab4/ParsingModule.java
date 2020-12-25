07e607546d03348d0b3736f14a67ef6ae7b6a2bf
Griffire <eaglegriff@mail.ru>
2020-12-25 18:13:05 +0300

    Scripted auto-commit on change (2020-12-25 18:13:05) by gitwatch.sh

--- first_lab/src/main/java/lab4/ParsingModule.java
+++ first_lab/src/main/java/lab4/ParsingModule.java
@@ -3,6 +3,9 @@ package lab4;
 import akka.actor.ActorRef;
 import akka.http.javadsl.server.Route;
 import akka.http.javadsl.marshallers.jackson.Jackson;
+
+import java.util.concurrent.Future;
+
 import static akka.http.javadsl.server.Directives.*;
 
 
@@ -16,6 +19,8 @@ public class ParsingModule {
 
     public Route newRouter (){
         Route r;
-        r = get(()-> parameter("", (p) -> ) )
+        r = get(()-> parameter("", (p) -> {
+            Future
+        }) )
     }
 }
