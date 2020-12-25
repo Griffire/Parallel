225d4a1f936af2a8926bb09322d4ae4a03ef0753
Griffire <eaglegriff@mail.ru>
2020-12-25 23:17:21 +0300

    Scripted auto-commit on change (2020-12-25 23:17:21) by gitwatch.sh

--- first_lab/src/main/java/lab4/StoreActor.java
+++ first_lab/src/main/java/lab4/StoreActor.java
@@ -13,11 +13,6 @@ public class StoreActor extends AbstractActor {
     private Map<String, ArrayList<String>> actorStorage;
 
 
-    @Override
-    public Receive createReceive() {
-        return null;
-    }
-
     public StoreActor() {
         this.log = Logging.getLogger(getContext().getSystem(), self());;
         this.actorStorage =  new HashMap<>();
