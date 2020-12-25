c43fff61e19de9a984a8d145ac79f65214316e45
Griffire <eaglegriff@mail.ru>
2020-12-25 20:18:11 +0300

    Scripted auto-commit on change (2020-12-25 20:18:11) by gitwatch.sh

--- first_lab/src/main/java/lab4/RouterActor.java
+++ first_lab/src/main/java/lab4/RouterActor.java
@@ -2,7 +2,8 @@ package lab4;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
-
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
 public class RouterActor extends AbstractActor {
     private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), self());
