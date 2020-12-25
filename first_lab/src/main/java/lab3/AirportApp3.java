fd3038f6e4cb12f1dd5262b9b6bf39f07bd7ac11
Griffire <eaglegriff@mail.ru>
2020-12-25 19:55:41 +0300

    Scripted auto-commit on change (2020-12-25 19:55:41) by gitwatch.sh

--- first_lab/src/main/java/lab3/AirportApp3.java
+++ first_lab/src/main/java/lab3/AirportApp3.java
@@ -16,46 +16,29 @@ import java.util.Map;
 public class AirportApp3 {
 
     public static String AIRPORT_TIMES_SPLIT = "[ ,]";
-    public static String AIRPORT_ID_SPLIT = "[,]";
-
+    public static String AIRPORT_ID_SPLIT = ",";
+    private static final int NAME_PAR = 11;
+    private static final int TIME_PAR = 14;
+    private static final int DELAY_PAR = 18;
+    private static final int DELAY_FL_PAR = 19;
 
     public static void main(String[] args)  {
         JavaSparkContext sc;
 
         SparkConf sparkConf = new SparkConf().setAppName("AirportApp3");
         sc = new JavaSparkContext(sparkConf);
-     //   String inputFile = "";
         JavaRDD<String> AIRPORT_TIMES = sc.textFile( "AIRPORT_TIMES.csv");
-//                flatMap(s -> Arrays.stream(s.split( AIRPORT_TIMES_SPLIT)).iterator());
-
         JavaRDD<String> AIRPORT_ID = sc.textFile( "L_AIRPORT_ID.csv");
-//                flatMap(s -> Arrays.stream(s.split( AIRPORT_ID_SPLIT)).iterator());;
-
         String Airp = AIRPORT_ID.first();
         JavaRDD<String> fAIRPORT_ID = AIRPORT_ID.filter(s -> !s.equals(Airp));
-
         String AirpTimes = AIRPORT_TIMES.first();
         JavaRDD<String> fAIRPORT_TIMES = AIRPORT_TIMES.filter(s -> !s.equals(AirpTimes));
-
-
-
-//        JavaPairRDD<String,Long> dist1 = AIRPORT_TIMES.zipWithIndex();
-//         fAIRPORT_TIMES = dist1.filter(s->s._2() >= 23).keys();
-
-//        JavaPairRDD<String,Long> dist2 = AIRPORT_TIMES.zipWithIndex();
-//        JavaRDD<String> fAIRPORT_ID = dist2.filter(s->s._2() >= 2).keys();
-
-
         Map<String,String> AIRPORT_NAMES = fAIRPORT_ID.map(str -> str.split("\"")).mapToPair(str -> new Tuple2<>(str[1],str[3])).collectAsMap();
-
-
         final Broadcast<Map<String, String>> airportsBroadcasted =
                 sc.broadcast(AIRPORT_NAMES);
-
-        JavaPairRDD<Tuple2<String,String>, Times> fDelay = fAIRPORT_TIMES.map( s -> s.split(",")).mapToPair(strings ->
-                new Tuple2<>(new Tuple2<>(strings[11], strings[14]),
-                new Times(strings[18],strings[19]))).groupByKey().mapValues(s -> new Times().counting(s));
-
+        JavaPairRDD<Tuple2<String,String>, Times> fDelay = fAIRPORT_TIMES.map( s -> s.split(AIRPORT_ID_SPLIT)).mapToPair(strings ->
+                new Tuple2<>(new Tuple2<>(strings[NAME_PAR], strings[TIME_PAR]),
+                new Times(strings[DELAY_PAR],strings[DELAY_FL_PAR]))).groupByKey().mapValues(s -> new Times().counting(s));
         JavaRDD<String> output = fDelay.map(s -> {
             String str = "NameFrom : " + airportsBroadcasted.value().get(s._1._1) + " NameTo" +
                     airportsBroadcasted.value().get(s._1._2) + " AverageDelay : " + s._2.getDelay() +
@@ -63,8 +46,6 @@ public class AirportApp3 {
             return str;
                  }) ;
         output.saveAsTextFile("output");
-
-
     }
 
 }
