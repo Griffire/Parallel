136736ba610cda24a78bff2402cd0c1a556577a1
Griffire <eaglegriff@mail.ru>
2020-12-25 18:04:37 +0300

    Scripted auto-commit on change (2020-12-25 18:04:37) by gitwatch.sh

--- first_lab/src/main/java/lab4/ParsingModule.java
+++ first_lab/src/main/java/lab4/ParsingModule.java
@@ -1,8 +1,10 @@
 package lab4;
 
+import akka.actor.ActorRef;
+
 public class ParsingModule {
 
-    public class ParsingModule {
+    public class ParsingModule(ActorRef ) {
         this.router =
     }
 }
