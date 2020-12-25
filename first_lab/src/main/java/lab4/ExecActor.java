e94f737968dcbd2f7462d4a0eb5da21d1bb8bff8
Griffire <eaglegriff@mail.ru>
2020-12-25 23:24:53 +0300

    Scripted auto-commit on change (2020-12-25 23:24:53) by gitwatch.sh

--- first_lab/src/main/java/lab4/ExecActor.java
+++ first_lab/src/main/java/lab4/ExecActor.java
@@ -1,4 +1,18 @@
 package lab4;
 
-public class ExecActor extends   {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+public class ExecActor extends AbstractActor {
+    private void send(MessageP msg){
+        sender().tell(new MessageP(msg.getIdS(),actorStorage.get(msg.getIdS())), getContext().parent());
+    }
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(MessageP.class, this::insert)
+                .match(MessageP.class, this::send)
+                .matchAny(o -> log.info("unknown res"))
+                .build();    }
 }
