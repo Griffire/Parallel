81b89d2a19b1708f8cc1cf13aaebb1a51e4dd409
Griffire <eaglegriff@mail.ru>
2020-12-25 20:32:48 +0300

    Scripted auto-commit on change (2020-12-25 20:32:48) by gitwatch.sh

--- first_lab/src/main/java/lab4/ExecuteActor.java
+++ first_lab/src/main/java/lab4/ExecuteActor.java
@@ -1,9 +1,10 @@
 package lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class ExecuteActor  extends AbstractActor {
     public Receive createReceive() {
-        
+        return ReceiveBuilder.create().match()
     }
 }
