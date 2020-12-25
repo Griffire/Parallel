33a20e5a404daa97c1b1e4ad4adaa0a0e05629b7
Griffire <eaglegriff@mail.ru>
2020-12-25 18:14:30 +0300

    Scripted auto-commit on change (2020-12-25 18:14:30) by gitwatch.sh

--- first_lab/src/main/java/lab4/ParsingModule.java
+++ first_lab/src/main/java/lab4/ParsingModule.java
@@ -4,6 +4,7 @@ import akka.actor.ActorRef;
 import akka.http.javadsl.server.Route;
 import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.pattern.Patterns;
+import okio.Timeout;
 
 import java.util.concurrent.Future;
 import java.util.regex.Pattern;
@@ -21,7 +22,7 @@ public class ParsingModule {
 
     public Route newRouter (){
         Route r;
-        
+        Timeout t1 = 
         r = get(()-> parameter("", (p) -> {
             Future<Object> f = Patterns.ask(this.router, , t)
         }) )
