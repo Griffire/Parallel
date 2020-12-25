61be5f3429bc0851bc4e08c7f55597ef8735d857
Griffire <eaglegriff@mail.ru>
2020-12-26 01:32:38 +0300

    Scripted auto-commit on change (2020-12-26 01:32:38) by gitwatch.sh

--- first_lab/src/main/java/lab4/NetworkApp.java
+++ first_lab/src/main/java/lab4/NetworkApp.java
@@ -1,8 +1,14 @@
 package lab4;
 
+import akka.actor.ActorSystem;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
 public class NetworkApp {
     public static void main(String[] args) {
-        
+        ActorSystem s1 = ActorSystem.create("test4");
+        LoggingAdapter log = Logging.getLogger(s1, System.out);
+        log.info("Server start");
 
     }
 }
