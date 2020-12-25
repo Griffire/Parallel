f4b94ce8972c871602bdbc404b824429be11f6f2
Griffire <eaglegriff@mail.ru>
2020-12-25 23:24:59 +0300

    Scripted auto-commit on change (2020-12-25 23:24:59) by gitwatch.sh

--- first_lab/src/main/java/lab4/ExecActor.java
+++ first_lab/src/main/java/lab4/ExecActor.java
@@ -3,7 +3,13 @@ package lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+
+
+
 public class ExecActor extends AbstractActor {
+
+
+    
     private void send(MessageP msg){
         sender().tell(new MessageP(msg.getIdS(),actorStorage.get(msg.getIdS())), getContext().parent());
     }
