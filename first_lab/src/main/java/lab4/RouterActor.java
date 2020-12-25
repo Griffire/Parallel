868c491ac33c6c50e8f4607287830b224435968e
Griffire <eaglegriff@mail.ru>
2020-12-25 20:15:13 +0300

    Scripted auto-commit on change (2020-12-25 20:15:13) by gitwatch.sh

--- first_lab/src/main/java/lab4/RouterActor.java
+++ first_lab/src/main/java/lab4/RouterActor.java
@@ -5,7 +5,7 @@ import akka.actor.AbstractActor;
 public class RouterActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(.class, this::executeTests)
+                .match(dataP.class, this::executeTests)
                 .match(MessageP.class, r -> storage.tell(r, sender()))
                 .matchAny(o -> log.info("recieved unknown message"))
                 .build();
