8a0f1651bf45a1f622d1a2d43f906a07f58ff53e
Griffire <eaglegriff@mail.ru>
2020-12-25 22:42:46 +0300

    Scripted auto-commit on change (2020-12-25 22:42:46) by gitwatch.sh

--- first_lab/src/main/java/lab4/StoreActor.java
+++ first_lab/src/main/java/lab4/StoreActor.java
@@ -2,9 +2,14 @@ package lab4;
 import akka.actor.AbstractActor;
 import akka.event.LoggingAdapter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class StoreActor extends AbstractActor {
     private LoggingAdapter log;
+    private Map<String, ArrayList<String>> actorStorage = new HashMap<>();
     
 
 }
