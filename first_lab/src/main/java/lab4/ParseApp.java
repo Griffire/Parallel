ac39dfbfa68407cb570081f9024f5b398e40be11
Griffire <eaglegriff@mail.ru>
2020-12-25 17:51:02 +0300

    Scripted auto-commit on change (2020-12-25 17:51:02) by gitwatch.sh

--- first_lab/src/main/java/lab4/ParseApp.java
+++ first_lab/src/main/java/lab4/ParseApp.java
@@ -3,6 +3,7 @@ package lab4;
 import akka.actor.*;
 import akka.*;
 import akka.routing.RouterActor;
+import org.apache.http.protocol.HTTP;
 
 
 public class ParseApp {
@@ -10,7 +11,7 @@ public class ParseApp {
     public static void main(String[] args) throws Exception{
         ActorSystem s1 = ActorSystem.create("test1");
         ActorRef routerActor = s1.actorOf(Props.create(RouterActor.class), "Router1");
-        
+        HTTP 
 
     }
 }
