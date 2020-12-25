9974ad8ce8b337ca53230185bec78b9fa58af0c7
Griffire <eaglegriff@mail.ru>
2020-12-25 22:31:04 +0300

    Scripted auto-commit on change (2020-12-25 22:31:04) by gitwatch.sh

--- first_lab/src/main/java/lab4/dataP.java
+++ first_lab/src/main/java/lab4/dataP.java
@@ -11,7 +11,7 @@ public class dataP {
     @JsonProperty("tests") ArrayList<testP> tests;
 
 
-    @JsonCreator dataP(String idS, ArrayList<testP> tests, ){
+    @JsonCreator dataP(String idS, ArrayList<testP> tests, String function , String ){
         this.idS = idS;
         this.tests = tests;
     }
