5048a45958b4a80a90db5f837b95ecd67fbe8045
Griffire <eaglegriff@mail.ru>
2020-12-25 22:32:18 +0300

    Scripted auto-commit on change (2020-12-25 22:32:18) by gitwatch.sh

--- first_lab/src/main/java/lab4/dataP.java
+++ first_lab/src/main/java/lab4/dataP.java
@@ -9,12 +9,27 @@ import java.util.ArrayList;
 public class dataP {
     @JsonProperty("idS") String idS;
     @JsonProperty("tests") ArrayList<testP> tests;
+    @JsonProperty("function") String function;
+    @JsonProperty("jsS") String jsS;
 
 
-    @JsonCreator dataP(String idS, ArrayList<testP> tests, String function , String ){
+
+    @JsonCreator dataP(String idS, ArrayList<testP> tests, String function , String jsS){
         this.idS = idS;
         this.tests = tests;
+        this.function = function;
+        this.jsS = jsS;
     }
+
+
+    public String getFunction() {
+        return this.function;
+    }
+
+    public String getJsS() {
+        return this.jsS;
+    }
+
 
     String getIdS(){
         return this.idS;
