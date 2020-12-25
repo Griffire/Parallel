162031ddf6c0d767aa637f61b9b38d809b73bbdf
Griffire <eaglegriff@mail.ru>
2020-12-25 19:25:27 +0300

    Scripted auto-commit on change (2020-12-25 19:25:27) by gitwatch.sh

--- first_lab/src/main/java/lab4/ParseApp.java
+++ first_lab/src/main/java/lab4/ParseApp.java
@@ -3,11 +3,11 @@ package lab4;
 import akka.actor.*;
 import akka.http.*;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.routing.RouterActor;
 import akka.stream.Materializer;
 import akka.stream.javadsl.Flow;
-import org.apache.http.HttpRequest;
 import org.apache.http.protocol.HTTP;
 import org.spark_project.jetty.http.HttpParser;
 import akka.NotUsed;
@@ -21,8 +21,8 @@ public class ParseApp {
         ActorRef r1Actor = s1.actorOf(Props.create(RouterActor.class), "Router1");
         Http http = Http.get(s1);
         ParsingModule PM = new ParsingModule(r1Actor);
-        Materializer m1 = Materializer.createMaterializer()
-        Flow<HttpRequest, HttpResponse,NotUsed> r1Flow = PM.newRouter().flow(s1,)
+        Materializer m1 = Materializer.createMaterializer(s1);
+        Flow<HttpRequest, HttpResponse,NotUsed> r1Flow = PM.newRouter().flow(s1,m1);
 
     }
 }
