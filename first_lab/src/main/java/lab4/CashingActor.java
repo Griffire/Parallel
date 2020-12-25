08933cb900c08f5688d94c53209a81cf9649159e
Griffire <eaglegriff@mail.ru>
2020-12-26 01:40:40 +0300

    Scripted auto-commit on change (2020-12-26 01:40:40) by gitwatch.sh

--- first_lab/src/main/java/lab4/CashingActor.java
+++ first_lab/src/main/java/lab4/CashingActor.java
@@ -14,6 +14,6 @@ public class CashingActor extends AbstractActor {
     @Override
     public Receive createReceive() {
 //        return null;
-        return ReceiveBuilder.create().match()
+        return ReceiveBuilder.create().match(String.class, msg -> )
     }
 }
