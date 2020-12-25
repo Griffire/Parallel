a10f0d98f949eb34a1178517eeca62c875b3e60b
Griffire <eaglegriff@mail.ru>
2020-12-25 19:20:24 +0300

    Scripted auto-commit on change (2020-12-25 19:20:24) by gitwatch.sh

--- first_lab/src/main/java/lab4/ParseApp.java
+++ first_lab/src/main/java/lab4/ParseApp.java
@@ -18,7 +18,7 @@ public class ParseApp {
         ActorRef r1Actor = s1.actorOf(Props.create(RouterActor.class), "Router1");
         Http http = Http.get(s1);
         ParsingModule PM = new ParsingModule(r1Actor);
-        Flow<HttpRequest, HttpResponse, >
+        Flow<HttpRequest, HttpResponse,  >
 
     }
 }
