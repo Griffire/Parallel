6ee325cf7aa2d2ea06665967f1cb135882480183
Griffire <eaglegriff@mail.ru>
2020-12-25 23:11:40 +0300

    Scripted auto-commit on change (2020-12-25 23:11:40) by gitwatch.sh

--- first_lab/src/main/java/lab4/MessageP.java
+++ first_lab/src/main/java/lab4/MessageP.java
@@ -3,7 +3,8 @@ package lab4;
 import java.util.ArrayList;
 
 public class MessageP {
-    private final String idS, res;
+    private  String idS, res;
+    private  ArrayList<String> results;
 
 
     public String getIdS() {
@@ -13,6 +14,7 @@ public class MessageP {
     MessageP(String idS){
         this.idS = idS;
         this.res = "none";
+        this.results = null;
     }
 
     public String getRes() {
@@ -22,6 +24,17 @@ public class MessageP {
     MessageP(String idS, String res){
         this.idS = idS;
         this.res = res;
+        this.results = null;
+    }
+
+    public ArrayList<String> getResults() {
+        return results;
+    }
+
+    MessageP(String idS, ArrayList<String> results){
+        this.idS = idS;
+        this.res = "none";
+        this.results = results;
     }
 
 
