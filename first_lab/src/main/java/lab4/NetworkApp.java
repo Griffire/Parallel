a587d5278a8fe53fb4f56acf7582b6f777733ffb
Griffire <eaglegriff@mail.ru>
2020-12-26 14:01:30 +0300

    Scripted auto-commit on change (2020-12-26 14:01:30) by gitwatch.sh

--- first_lab/src/main/java/lab4/NetworkApp.java
+++ first_lab/src/main/java/lab4/NetworkApp.java
@@ -22,7 +22,7 @@ public class NetworkApp {
 
     static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer m1, ActorRef act){
         Flow r;
-        r =
+        r = Flow.of(HttpRequest.class).
     }
 
 
