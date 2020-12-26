bda125381fd8da0e7ac8ae835c91ae903ee95698
Griffire <eaglegriff@mail.ru>
2020-12-26 14:09:25 +0300

    Scripted auto-commit on change (2020-12-26 14:09:25) by gitwatch.sh

--- first_lab/src/main/java/lab4/NetworkApp.java
+++ first_lab/src/main/java/lab4/NetworkApp.java
@@ -25,7 +25,7 @@ public class NetworkApp {
         Flow r;
         r = Flow.of(HttpRequest.class).map(o -> {
             return new Pair<>(o.getUri().query().getOrElse(url,host),o.getUri().query().getOrElse(COUNT, "1") );
-        }).mapAsync(2, (Pair<String,Integer> pair));
+        }).mapAsync(2, (Pair<String,Integer> pair) -> );
     }
 
 
