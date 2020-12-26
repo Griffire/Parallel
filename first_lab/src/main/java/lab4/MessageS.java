f55206dafabd0d686d490648e6b278fcb0a3c292
Griffire <eaglegriff@mail.ru>
2020-12-26 13:35:34 +0300

    Scripted auto-commit on change (2020-12-26 13:35:34) by gitwatch.sh

--- first_lab/src/main/java/lab4/MessageS.java
+++ first_lab/src/main/java/lab4/MessageS.java
@@ -3,5 +3,18 @@ package lab4;
 public class MessageS {
     private String adress;
     private  float time;
-    
+
+
+    public String getAdress() {
+        return this.adress;
+    }
+
+    public float getTime() {
+        return this.time;
+    }
+
+    public MessageS(String adress, float time) {
+        this.adress = adress;
+        this.time = time;
+    }
 }
